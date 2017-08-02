6e3c87f0e696e6b29579577bd81b1c75abe0f125
Ng Shyue Jer <ngshyuejer@gmail.com>
2017-08-02 23:45:22 +0800

    add request HEAD and its summary

--- sector_scrape.py
+++ sector_scrape.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://www.isaham.my/all-sectors'
+url_2 = 'http://www.isaham.my/sector'
 
 # want to better show that I'm accessing the SUBSECTION.
 print "accesing " + url + "..."
@@ -20,11 +21,28 @@ for cell in soup.findAll("a", class_="list-group-item"):
     cell = cell.text
     cell = cell.replace(u'\xa0', u' ')
     list_sectors.append(cell)
+# These are my requests counters. 
+good_counter = 0
+bad_counter = 0  
 
-print list_sectors
-
+# Checking if website links work
 for sector in list_sectors:
-    print url + '/' + sector
+    sector = sector[:-1].lower()
+    sector = sector.split(" ")
+    sector = "-".join(sector)
+    sector = sector.replace('&','and')
+    request = requests.head(url_2 + '/' + sector)
+    if request.status_code == 200:
+        print('Website exists for %s' %sector)
+        good_counter += 1
+    else:
+        print('Website does not exist for %s' %sector)
+        bad_counter += 1
+
+
+print "Number of good counters: %i" %good_counter
+print "Number of bad counters: %i" %bad_counter        
+
 '''
 for row in soup.findAll('tr')[1:]:
     data_cells = []
@@ -33,3 +51,6 @@ for row in soup.findAll('tr')[1:]:
         data_cells.append(text)
     data_rows.append(data_cells)
 '''    
+
+# Now I need to check if all the websites are in fact functioning
+
